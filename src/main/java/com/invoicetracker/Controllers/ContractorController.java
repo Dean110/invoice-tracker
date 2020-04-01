48335df8dc3870894be66ee6665bd6ecae74a7e7
mcheich <mcheich@gmail.com>
2020-04-01 13:54:23 -0400

    has contractor controller and 2 tests

--- src/main/java/com/invoicetracker/Controllers/ContractorController.java
+++ src/main/java/com/invoicetracker/Controllers/ContractorController.java
@@ -6,10 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 import com.invoicetracker.Repositories.ContractorRepository;
 
-@Controller
 @RequestMapping("/contractor")
+@Controller
 public class ContractorController {
 	
 	@Resource
